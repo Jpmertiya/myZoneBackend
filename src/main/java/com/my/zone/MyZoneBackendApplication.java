160fb8c8371e28379eec7ec198d32ed56b1bbbba
JASPAL SINGH <jpmertiya57@gmail.com>
2024-01-12 22:10:03 +0530

    this is my backend api for myZone project.

---
+++ src/main/java/com/my/zone/MyZoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.my.zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyZoneBackendApplication.class, args);
+	}
+
+}
